b915f857ad6474839c68010500ca9e47cf3380f4
Siddhartha <siddharthapaul101@gmail.com>
2024-03-25 14:17:48 +0530

    setup employee profile screen

---
+++ src/app/employee-profile/employee-profile.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../model/employee';
+import { ActivatedRoute } from '@angular/router';
+import { EmployeeService } from '../employee.service';
+
+@Component({
+  selector: 'app-employee-profile',
+  templateUrl: './employee-profile.component.html',
+  styleUrls: ['./employee-profile.component.css']
+})
+export class EmployeeProfileComponent implements OnInit {
+  employee: Employee = {} as Employee;
+
+  constructor(private route: ActivatedRoute,
+              private employeeService: EmployeeService) {}
+
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id') as string;
+    console.log('Employee id ', id);
+    this.getEmployeeByEmpId(id);
+  }
+
+  getEmployeeByEmpId(id: string) {
+    this.employeeService.getEmployeeById(id).subscribe((data) => {
+      this.employee = data;
+    });
+  }
+}
