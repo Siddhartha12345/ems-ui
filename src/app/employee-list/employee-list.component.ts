e2d41569c4ca4c0feb821f3b349ccf6c84803797
Siddhartha <siddharthapaul101@gmail.com>
2024-03-29 18:46:26 +0530

    modal template implementation

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from '../model/employee';
 import { EmployeeService } from 'src/services/employee.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+declare var window: any;
 
 @Component({
   selector: 'app-employee-list',
@@ -11,11 +14,30 @@ export class EmployeeListComponent implements OnInit {
 
   employeeList: Employee[] = [];
 
-  constructor(private employeeService: EmployeeService) {}
+  // modal
+  formModal: any;
+  
+  // form
+  registerForm: FormGroup;
+
+  constructor(private employeeService: EmployeeService,
+              private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
     // populating the employe list array with data
     this.getEmployeeList();
+
+    // modal
+    this.formModal = new window.bootstrap.Modal(
+      document.getElementById('myModal')
+    );
+
+    // form initialization
+    this.registerForm = this.formBuilder.group({
+      firstName: [''],
+      maritalStatus: [''],
+      gender: ['']
+    });
   }
 
   getEmployeeList() {
@@ -24,5 +46,18 @@ export class EmployeeListComponent implements OnInit {
         this.employeeList = data;
       }
     );
+  }
+
+  // modal
+  openFormModal() {
+    // display the modal
+    this.formModal.show();
+  }
+
+  // form on submit
+  onSubmit() {
+    console.log('Form value: ', this.registerForm.value);
+    this.formModal.hide();
+    this.registerForm.reset();
   }
 }
